9597d496b08d4417414ac6358f2a8cb0b732a78c
sonamanayakkar <esonaman2@gmail.com>
2026-04-02 17:46:43 +0530

    flight api check2

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://your-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.get("/api/flights", async (req, res) => {
     try {
