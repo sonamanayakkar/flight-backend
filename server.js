eef77c17a7eb6d7c7dde6293da08165f5b9fbf15
sonamanayakkar <esonaman2@gmail.com>
2026-04-02 18:22:33 +0530

    flight api check4

--- server.js
+++ server.js
@@ -8,32 +8,40 @@ app.use(cors());
 app.use(express.json())
 
 app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://your-frontend.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
+    //   res.header("Access-Control-Allow-Origin", "https://your-frontend.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
 
-  next();
+    next();
 });
 
 app.get("/api/flights", async (req, res) => {
     try {
-        let response = await axios.get("https://opensky-network.org/api/states/all");
+        let response = await axios.get("https://opensky-network.org/api/states/all",
+            {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                },
+                timeout: 5000
+            }
+        );
         // let response = await fetch("https://countriesnow.space/api/v0.1/countries/positions");
 
         // response = await response.json();
-
+   
+        
         res.json({
             data: response.data
         });
 
     } catch (err) {
         console.error("Flight API Error:", err.message);
-        res.status(500).json({ error: "Failed",details: err.message });
+        res.status(500).json({ error: "Failed", details: err.message });
     }
 });
 
